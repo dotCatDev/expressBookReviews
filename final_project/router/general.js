8116e1d1073f4ec946fdaf108c7e221057e5d7c1
Sebastian <sebastian.quinones.inf@gmail.com>
2024-07-27 22:14:14 -0400

    Second Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,26 +17,56 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Retrieve the email parameter from the request URL and send the corresponding friend's details
+   const isbn = req.params.isbn;
+   res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const author = req.params.author;
+
+    var result = Object.values(books).filter(book => book.author == author);
+
+    res.send(result);
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+
+    var result = Object.values(books).filter(book => book.title == title);
+
+    res.send(result);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+
+    const filtered = Object.fromEntries(
+        Object.entries(books).filter(
+           ([key, val])=> key == isbn
+        )
+     );
+
+     var valuechecked = "reviews";
+
+     for (var k in filtered){
+        console.log(k);
+       if(k ==  1)
+       {
+         var entry = filtered[k];
+         for (var a in entry){
+           console.log(entry[a]);
+         }
+     
+       }                           
+     }
+      
+    res.send(filtered);
 });
 
 module.exports.general = public_users;
